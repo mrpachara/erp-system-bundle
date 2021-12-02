bdbc49372532965d7f07a613be4d441482a1a38b
Pachara Tinamas <mrpachara@gmail.com>
2021-12-02 22:39:36 +0700

    Update method to match the new interface.

--- Infrastructure/ORM/Service/SystemAccountQuery.php
+++ Infrastructure/ORM/Service/SystemAccountQuery.php
@@ -2,6 +2,7 @@
 
 namespace Erp\Bundle\SystemBundle\Infrastructure\ORM\Service;
 
+use Doctrine\ORM\QueryBuilder;
 use Erp\Bundle\SystemBundle\Domain\CQRS\SystemAccountQuery as QueryInterface;
 use Erp\Bundle\CoreBundle\Infrastructure\ORM\Service\CoreAccountQuery as ParentQuery;
 
@@ -17,30 +18,30 @@ abstract class SystemAccountQuery extends ParentQuery implements QueryInterface
 
         return $result;
     }
-    
+
     /**
      * @var SystemAccountQueryHelper
      */
     protected $systemAccountQh;
-    
+
     /** @required */
     public function setSystemAccountQueryHelper(SystemAccountQueryHelper $systemAccountQh)
     {
         $this->systemAccountQh = $systemAccountQh;
     }
-    
+
     public function findAll()
     {
         $alias = '_entity';
         $qb = $this->systemAccountQh->exceptSpecial($this->repository->createQueryBuilder($alias), $alias);
-        
+
         return $qb->getQuery()->getResult();
     }
-    
-    public function searchQueryBuilder(array $params, string $alias = null, &$context = null)
+
+    public function searchQueryBuilder(array $params, string $alias, &$context = null) : QueryBuilder
     {
         $alias = (empty($alias))? '_entity' : $alias;
-        
+
         return $this->systemAccountQh->exceptSpecial(parent::searchQueryBuilder($params, $alias, $context), $alias);
     }
 }
